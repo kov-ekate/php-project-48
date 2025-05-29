dbc5a303fcacc8056b40a27c2bfc4c1aaedb87a6
Екатерина Коврижко <eh1306@yandex.ru>
2025-05-29 16:06:44 +0300

    fix

--- test.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require __DIR__ . '/vendor/autoload.php';
-
-use function Differ\Differ\genDiff;
-
-$diff = genDiff('./tests/fixtures/file1.json', './tests/fixtures/file2.json', 'json');
-print_r($diff);
