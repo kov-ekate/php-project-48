8ec866fd132099d32be5413ba22a6c6d88403dbf
Екатерина Коврижко <eh1306@yandex.ru>
2025-05-29 16:04:55 +0300

    fix json

--- src/Formatters/Json.php
+++ src/Formatters/Json.php
@@ -2,7 +2,7 @@
 
 namespace Formatters\Json;
 
-function toString(mixed $value): string|false
+function toString(mixed $value): string
 {
     if ($value === null) {
         return 'null';
@@ -20,98 +20,167 @@ function json(array $diff): string
 
     $iter = function ($currentValue, $depth) use (&$iter, $replacer, $spacesCount) {
         if (!is_array($currentValue)) {
-            return toString($currentValue);
+            return [toString($currentValue)];
         }
 
         $lines = '';
 
-        foreach ($currentValue as $item) {
+        $callback = function ($acc, $item) use ($iter, $depth, $replacer, $spacesCount) {
             $beginIndent = str_repeat($replacer, $depth * $spacesCount);
             $indent = str_repeat($replacer, $depth * $spacesCount + $spacesCount);
             switch ($item['type']) {
                 case 'add':
                     if (is_array($item['value'])) {
-                        $lines .= $beginIndent . "{\n";
-                        $lines .= $indent . '"type": "added"' . ",\n";
-                        $lines .= $indent . '"key": ' . toString($item['key']) . ",\n";
-                        $lines .= $indent . '"value": ' . "[\n" . $iter(($item['value']), $depth + 2);
-                        $lines = trim(rtrim($lines), ',');
-                        $lines .= "\n" . $indent . "]\n";
-                        $lines .= $beginIndent . "},\n";
+                        $result1 = $indent . '"type": "added",';
+                        $result2 = $indent . '"key": ' . toString($item['key']) . ",";
+                        $result3 = $indent . '"value": [';
+                        $result4 = $iter(($item['value']), $depth + 2);
+                        $count = count($result4);
+                        $res = array_slice($result4, 0, $count - 1);
+                        $lastElement = end($result4);
+                        $trimmedLastElement = rtrim($lastElement, ',');
+                        $newResult4 = [...$res, $trimmedLastElement];
+                        $result5 = $indent . "]";
+                        return [
+                            ...$acc,
+                            $beginIndent . "{",
+                            $result1,
+                            $result2,
+                            $result3,
+                            ...$newResult4,
+                            $result5,
+                            $beginIndent . "},"
+                        ];
                     } else {
-                        $lines .= $beginIndent . "{\n";
-                        $lines .= $indent . '"type": "added"' . ",\n";
-                        $lines .= $indent . '"key": ' . toString($item['key']) . ",\n";
-                        $lines .= $indent . '"value": ' . toString($item['value']) . "\n";
-                        $lines .= $beginIndent . "},\n";
+                        $result1 = $indent . '"type": "added",';
+                        $result2 = $indent . '"key": ' . toString($item['key']) . ",";
+                        $result3 = $indent . '"value": ' . toString($item['value']);
+                        return [...$acc, $beginIndent . "{", $result1, $result2, $result3, $beginIndent . "},"];
                     }
-                    break;
                 case 'delete':
                     if (is_array($item['value'])) {
-                        $lines .= $beginIndent . "{\n";
-                        $lines .= $indent . '"type": "deleted"' . ",\n";
-                        $lines .= $indent . '"key": ' . toString($item['key']) . ",\n";
-                        $lines .= $indent . '"value": ' . "[\n" . $iter(($item['value']), $depth + 2);
-                        $lines = trim(rtrim($lines), ',');
-                        $lines .= "\n" . $indent . "]\n";
-                        $lines .= $beginIndent . "},\n";
+                        $result1 = $indent . '"type": "deleted",';
+                        $result2 = $indent . '"key": ' . toString($item['key']) . ",";
+                        $result3 = $indent . '"value": [';
+                        $result4 = $iter(($item['value']), $depth + 2);
+                        $count = count($result4);
+                        $res = array_slice($result4, 0, $count - 1);
+                        $lastElement = end($result4);
+                        $trimmedLastElement = rtrim($lastElement, ',');
+                        $newResult4 = [...$res, $trimmedLastElement];
+                        $result5 = $indent . "]";
+                        return [
+                            ...$acc,
+                            $beginIndent . "{",
+                            $result1,
+                            $result2,
+                            $result3,
+                            ...$newResult4,
+                            $result5,
+                            $beginIndent . "},"
+                        ];
                     } else {
-                        $lines .= $beginIndent . "{\n";
-                        $lines .= $indent . '"type": "deleted"' . ",\n";
-                        $lines .= $indent . '"key": ' . toString($item['key']) . ",\n";
-                        $lines .= $indent . '"value": ' . toString($item['value']) . "\n";
-                        $lines .= $beginIndent . "},\n";
+                        $result1 = $indent . '"type": "deleted",';
+                        $result2 = $indent . '"key": ' . toString($item['key']) . ",";
+                        $result3 = $indent . '"value": ' . toString($item['value']);
+                        return [...$acc, $beginIndent . "{", $result1, $result2, $result3, $beginIndent . "},"];
                     }
-                    break;
                 case 'nested':
-                    $lines .= $beginIndent . "{\n";
-                    $lines .= $indent . '"type": "nested"' . ",\n";
-                    $lines .= $indent . '"key": ' . toString($item['key']) . ",\n";
-                    $lines .= $indent . '"children": ' . "[\n" . $iter(($item['children']), $depth + 2);
-                    $lines = trim(rtrim($lines), ',');
-                    $lines .= "\n" . $indent . "]\n";
-                    $lines .= $beginIndent . "},\n";
-                    break;
+                    $result1 = $indent . '"type": "nested",';
+                    $result2 = $indent . '"key": ' . toString($item['key']) . ",";
+                    $result3 = $indent . '"children": [';
+                    $result4 = $iter(($item['children']), $depth + 2);
+                    $count = count($result4);
+                    $res = array_slice($result4, 0, $count - 1);
+                    $lastElement = end($result4);
+                    $trimmedLastElement = rtrim($lastElement, ',');
+                    $newResult4 = [...$res, $trimmedLastElement];
+                    $result5 = $indent . "]";
+                    return [
+                        ...$acc,
+                        $beginIndent . "{",
+                        $result1,
+                        $result2,
+                        $result3,
+                        ...$newResult4,
+                        $result5,
+                        $beginIndent . "},"
+                    ];
                 case 'unchange':
-                    $lines .= $beginIndent . "{\n";
-                    $lines .= $indent . '"type": "unchanged"' . ",\n";
-                    $lines .= $indent . '"key": ' . toString($item['key']) . ",\n";
-                    $lines .= $indent . '"value": ' . toString($item['value']) . "\n";
-                    $lines .= $beginIndent . "},\n";
-                    break;
+                    $result1 = $indent . '"type": "unchanged",';
+                    $result2 = $indent . '"key": ' . toString($item['key']) . ",";
+                    $result3 = $indent . '"value": ' . toString($item['value']);
+                    return [...$acc, $beginIndent . "{", $result1, $result2, $result3, $beginIndent . "},"];
                 case 'change':
                     $oldValue = $item['old value'];
                     $newValue = $item['new value'];
                     if (is_array($oldValue)) {
-                        $lines .= $beginIndent . "{\n";
-                        $lines .= $indent . '"type": "changed"' . ",\n";
-                        $lines .= $indent . '"key": ' . toString($item['key']) . ",\n";
-                        $lines .= $indent . '"old value": ' . "[\n" . $iter($oldValue, $depth + 2);
-                        $lines = trim(rtrim($lines), ',');
-                        $lines .= "\n" . $indent . "],\n";
-                        $lines .= $indent . '"new value": ' . toString($newValue) . "\n";
-                        $lines .= $beginIndent . "},\n";
+                        $result1 = $indent . '"type": "changed",';
+                        $result2 = $indent . '"key": ' . toString($item['key']) . ",";
+                        $result3 = $indent . '"old value": [';
+                        $result4 = $iter($oldValue, $depth + 2);
+                        $count = count($result4);
+                        $res = array_slice($result4, 0, $count - 1);
+                        $lastElement = end($result4);
+                        $trimmedLastElement = rtrim($lastElement, ',');
+                        $newResult4 = [...$res, $trimmedLastElement];
+                        $result5 = $indent . "],";
+                        $result6 = $indent . '"new value": ' . toString($newValue);
+                        return [
+                            ...$acc,
+                            $beginIndent . "{",
+                            $result1,
+                            $result2,
+                            $result3,
+                            ...$newResult4,
+                            $result5,
+                            $result6,
+                            $beginIndent . "},"
+                        ];
                     } elseif (is_array($newValue)) {
-                        $lines .= $beginIndent . "{\n";
-                        $lines .= $indent . '"type": "changed"' . ",\n";
-                        $lines .= $indent . '"key": ' . toString($item['key']) . ",\n";
-                        $lines .= $indent . '"old value": ' . toString($oldValue) . ",\n";
-                        $lines .= $indent . '"new value": ' . "[\n" . $iter($newValue, $depth + 2);
-                        $lines = trim(rtrim($lines), ',');
-                        $lines .= "\n" . $indent . "]\n";
-                        $lines .= $beginIndent . "},\n";
+                        $result1 = $indent . '"type": "changed",';
+                        $result2 = $indent . '"key": ' . toString($item['key']) . ",";
+                        $result3 = $indent . '"old value": ' . toString($oldValue);
+                        $result4 = $indent . '"new value": [';
+                        $result5 = $iter($newValue, $depth + 2);
+                        $count = count($result5);
+                        $res = array_slice($result5, 0, $count - 1);
+                        $lastElement = end($result5);
+                        $trimmedLastElement = rtrim($lastElement, ',');
+                        $newResult5 = [...$res, $trimmedLastElement];
+                        $result6 = $indent . "],";
+                        return [
+                            ...$acc,
+                            $beginIndent . "{",
+                            $result1,
+                            $result2,
+                            $result3,
+                            $result4,
+                            ...$newResult5,
+                            $result6,
+                            $beginIndent . "},"
+                        ];
                     } else {
-                        $lines .= $beginIndent . "{\n";
-                        $lines .= $indent . '"type": "changed"' . ",\n";
-                        $lines .= $indent . '"key": ' . toString($item['key']) . ",\n";
-                        $lines .= $indent . '"old value": ' . toString($oldValue) . ",\n";
-                        $lines .= $indent . '"new value": ' . toString($newValue) . "\n";
-                        $lines .= $beginIndent . "},\n";
+                        $result1 = $indent . '"type": "changed",';
+                        $result2 = $indent . '"key": ' . toString($item['key']) . ",";
+                        $result3 = $indent . '"old value": ' . toString($oldValue) . ",";
+                        $result4 = $indent . '"new value": ' . toString($newValue);
+                        return [
+                            ...$acc,
+                            $beginIndent . "{",
+                            $result1,
+                            $result2,
+                            $result3,
+                            $result4,
+                            $beginIndent . "},"
+                        ];
                     }
-                    break;
             }
-        }
-        return $lines;
+        };
+        $json = array_reduce($currentValue, $callback, []);
+        return $json;
     };
-    return "[\n" . rtrim($iter($diff, 1), "\n,") . "\n]";
+    $res = $iter($diff, 1);
+    $result = implode("\n", $res);
+    return "[\n" . trim($result, ",") . "\n]";
 }
--- test.php
+++ test.php
@@ -4,5 +4,5 @@ require __DIR__ . '/vendor/autoload.php';
 
 use function Differ\Differ\genDiff;
 
-$diff = genDiff('./tests/fixtures/file1.json', './tests/fixtures/file2.json');
+$diff = genDiff('./tests/fixtures/file1.json', './tests/fixtures/file2.json', 'json');
 print_r($diff);
