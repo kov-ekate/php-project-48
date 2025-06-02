2e5c99ed1b4e143e9322775cfd6cbd901c9493d3
Екатерина Коврижко <eh1306@yandex.ru>
2025-06-02 17:52:12 +0300

    fix psrser

--- src/Build/Parser.php
+++ src/Build/Parser.php
@@ -5,16 +5,22 @@ namespace Build\Parser;
 use Exception;
 use Symfony\Component\Yaml\Yaml;
 
-function readFile(string $pathToFile): string | false
+function readFile(string $pathToFile): string
 {
     $extension = pathinfo($pathToFile, PATHINFO_EXTENSION);
     if ($extension === 'json') {
-        return (string)file_get_contents($pathToFile);
+        $result = (string)file_get_contents($pathToFile);
     } elseif ($extension === 'yml' || $extension === 'yaml') {
-        return file_get_contents($pathToFile);
+        $result = file_get_contents($pathToFile);
     } else {
         throw new Exception('Invalid file format');
     }
+
+    if ($result === false) {
+        throw new Exception("Failed to read file {$pathToFile}");
+    }
+
+    return $result;
 }
 
 function parseJson(string $file): array
