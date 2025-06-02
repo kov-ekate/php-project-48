e1b1e6968eb2204d1de73b832fb1e88263def8fa
Екатерина Коврижко <eh1306@yandex.ru>
2025-06-02 17:37:43 +0300

    fix parser

--- src/Build/Parser.php
+++ src/Build/Parser.php
@@ -9,11 +9,13 @@ function readFile(string $pathToFile): string
 {
     $extension = pathinfo($pathToFile, PATHINFO_EXTENSION);
     if ($extension === 'json') {
-        return (string)file_get_contents($pathToFile);
+        $result = (string)file_get_contents($pathToFile);
     } elseif ($extension === 'yml' || $extension === 'yaml') {
-        return file_get_contents($pathToFile);
+        $result = file_get_contents($pathToFile);
+    } else {
+        throw new Exception('Invalid file format');
     }
-    throw new Exception('Invalid file format');
+    return $result;
 }
 
 function parseJson(string $file): array
