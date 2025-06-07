db958dab4f024d3613f185bd244790b1850da0ab
Екатерина Коврижко <eh1306@yandex.ru>
2025-06-07 11:33:13 +0300

    delete test

--- test.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require __DIR__ . '/vendor/autoload.php';
-
-use function Differ\Differ\genDiff;
-
-$result = genDiff('./tests/fixtures/file1.json', './tests/fixtures/file2.json');
-var_dump($result);
