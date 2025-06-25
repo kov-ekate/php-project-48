e62f1e12643e779d33bce418a93eb0672b181367
Екатерина Коврижко <eh1306@yandex.ru>
2025-06-25 13:53:43 +0300

    fix json

--- src/Formatters/Json.php
+++ src/Formatters/Json.php
@@ -2,7 +2,15 @@
 
 namespace Differ\Formatters\Json;
 
+use function Functional\retry;
+
 function format(array $diff): string
 {
-    return json_encode($diff, JSON_PRETTY_PRINT);
+    $result = json_encode($diff, JSON_PRETTY_PRINT);
+
+    if ($result === false) {
+        return "Error: " . json_last_error_msg();
+    }
+
+    return $result;
 }
